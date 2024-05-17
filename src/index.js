76b5f216493caa53eb11d872a0268cc5b556dfc5
Nikita Gonohov <gonohovn@gmail.com>
2024-05-17 16:38:32 +0300

    add basename to index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <Provider store={store}>
-        <BrowserRouter>
+        <BrowserRouter basename={process.env.PUBLIC_URL}>
             <App />
         </BrowserRouter>
     </Provider>
