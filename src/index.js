1404aee460b93b8ab7de4da4b0f236e8789657fe
Nikita Gonohov <gonohovn@gmail.com>
2024-05-17 17:33:07 +0300

    gh-pages update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <Provider store={store}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter basename={process.env.NODE_ENV === 'development' ? '/' : process.env.PUBLIC_URL}>
             <App />
         </BrowserRouter>
     </Provider>
